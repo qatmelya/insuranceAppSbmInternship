c1fbb582d9f14008a275620eaf906af13f4321d3
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2023-07-05 17:03:50 +0300

    Kasko Calculate form first try
    Form should post async ajax and redirect to calculation result page

--- src/main/java/com/sbm/application/controllers/InsuranceController.java
+++ src/main/java/com/sbm/application/controllers/InsuranceController.java
@@ -1,13 +1,19 @@
 package com.sbm.application.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbm.application.entities.concretes.City;
+import com.sbm.application.entities.concretes.Customer;
+import com.sbm.application.entities.concretes.Profession;
 
 @Controller
 @RequestMapping("/insurances")
@@ -19,12 +25,19 @@ public class InsuranceController {
 	public String calculate(@PathVariable String name, Model model) {
 		model.addAttribute("controller", controllerName);
 		model.addAttribute("page", name + "Calculate");
+		Customer customer = new Customer();
+		ArrayList<Profession> professions = new ArrayList<Profession>();
+		ArrayList<City> cities = new ArrayList<City>();
+		model.addAttribute("customer", customer);
+		model.addAttribute("professions", professions);
+		model.addAttribute("cities", cities);
+		
 		return "app";
 	}
 	
 	@PostMapping("/calculate/{name}")
 	@ResponseBody
-	public String calculate(@PathVariable String name) {
+	public String calculate(@ModelAttribute("customer") Customer customer, @PathVariable String name) {
 		return name;
 	}
 
