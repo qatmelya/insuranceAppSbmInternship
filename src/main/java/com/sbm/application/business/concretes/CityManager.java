36347523e65c3e9743f7175c64978196ab82069e
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2023-07-10 10:49:02 +0300

    city and profession business layer added

---
+++ src/main/java/com/sbm/application/business/concretes/CityManager.java
@@ -0,0 +1,69 @@
+package com.sbm.application.business.concretes;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sbm.application.business.abstracts.CityService;
+import com.sbm.application.core.utilities.results.DataResult;
+import com.sbm.application.core.utilities.results.ErrorDataResult;
+import com.sbm.application.core.utilities.results.ErrorResult;
+import com.sbm.application.core.utilities.results.Result;
+import com.sbm.application.core.utilities.results.SuccessDataResult;
+import com.sbm.application.core.utilities.results.SuccessResult;
+import com.sbm.application.entities.concretes.City;
+import com.sbm.application.repositories.concretes.CityRepository;
+
+@Service
+public class CityManager implements CityService {
+
+	@Autowired
+	private CityRepository cityRepository;
+
+	@Override
+	public Result save(City city) {
+		try {
+			if(city.getId()==0) {
+				cityRepository.save(city).block(Duration.ofSeconds(1));
+				return new SuccessResult("Şehir eklendi");
+			}
+			City foundCity = cityRepository.findById(city.getId()).block(Duration.ofSeconds(1));
+			if(foundCity == null) {
+				return new ErrorResult("%s idli Şehir bulunamadı".formatted(city.getId()));
+			}
+			cityRepository.save(city).block(Duration.ofSeconds(1));
+			return new SuccessResult("Şehir güncelleme başarılı!");
+		} catch (RuntimeException ex) {
+			return new ErrorResult("İstek zaman aşımına uğradı!");
+		}
+	}
+
+	@Override
+	public Result delete(City city) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<City> getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<City>> getAll() {
+		List<City> cities = new ArrayList<City>();
+		try {
+		  cityRepository.findAll().doOnNext(cities::add).blockLast(Duration.ofSeconds(10));
+		  return new SuccessDataResult<List<City>>(cities,"Başarılı");
+		}
+		catch(RuntimeException ex) {
+			System.out.println(ex.getMessage());
+			return new ErrorDataResult<List<City>>(cities,"İstek zaman aşımına uğradı!");
+		}
+	}
+
+}
---
+++ src/main/java/com/sbm/application/business/concretes/ProfessionManager.java
@@ -0,0 +1,54 @@
+package com.sbm.application.business.concretes;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sbm.application.business.abstracts.ProfessionService;
+import com.sbm.application.core.utilities.results.DataResult;
+import com.sbm.application.core.utilities.results.ErrorDataResult;
+import com.sbm.application.core.utilities.results.Result;
+import com.sbm.application.core.utilities.results.SuccessDataResult;
+import com.sbm.application.entities.concretes.Profession;
+import com.sbm.application.repositories.concretes.ProfessionRepository;
+
+@Service
+public class ProfessionManager implements ProfessionService {
+
+	@Autowired
+	private ProfessionRepository professionRepository;
+	@Override
+	public Result save(Profession entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result delete(Profession entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<Profession> getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<Profession>> getAll() {
+		List<Profession> professions = new ArrayList<Profession>();
+		try {
+			professionRepository.findAll().doOnNext(professions::add).blockLast(Duration.ofSeconds(10));
+		  return new SuccessDataResult<List<Profession>>(professions,"Başarılı");
+		}
+		catch(RuntimeException ex) {
+			System.out.println(ex.getMessage());
+			return new ErrorDataResult<List<Profession>>(professions,"İstek zaman aşımına uğradı!");
+		}
+	}
+
+}
