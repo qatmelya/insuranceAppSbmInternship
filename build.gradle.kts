09c815f95f4099abadd01c18d19ef15f5c27daae
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2023-07-10 17:04:35 +0300

    webjars added to grade build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,21 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.webjars:bootstrap:5.3.0")
+	implementation("org.webjars:popper.js:2.9.3")
+  compileOnly("org.webjars.npm:izitoast:1.4.0")
+	implementation("org.webjars:webjars-locator:0.47")
+	implementation("commons-validator:commons-validator:1.7")
+	
 	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
 	runtimeOnly("io.r2dbc:r2dbc-mssql:1.0.0.RELEASE")
+	
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	
+	compileOnly("org.projectlombok:lombok:1.18.28")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/sbm/application/Application.java
+++ src/main/java/com/sbm/application/Application.java
@@ -6,10 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-
 import io.r2dbc.spi.ConnectionFactory;
 
 @SpringBootApplication
