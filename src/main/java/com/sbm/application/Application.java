ab4359f32b42eed59bf00c125cdb302aa4d42e5d
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2023-07-17 13:57:42 +0300

    Remove unnecessary database initialization logging

--- src/main/java/com/sbm/application/Application.java
+++ src/main/java/com/sbm/application/Application.java
@@ -1,8 +1,6 @@
 package com.sbm.application;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +10,6 @@ import io.r2dbc.spi.ConnectionFactory;
 
 @SpringBootApplication
 public class Application {
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -20,10 +17,8 @@ public class Application {
 	
 	@Bean
     ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-		log.info("Database Connnection initializing...");
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
-		log.info("Database Connnection initialized!");
         return initializer;
     }
 	
