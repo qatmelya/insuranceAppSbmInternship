6c8d93b56196dca439f1f0b6c14ea2ac8158d43a
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2023-07-25 10:41:42 +0300

    repositories for real estates

---
+++ src/main/java/com/sbm/application/repositories/concretes/RealEstateUnitConstructionCostRepository.java
@@ -0,0 +1,11 @@
+package com.sbm.application.repositories.concretes;
+
+import org.springframework.stereotype.Repository;
+
+import com.sbm.application.entities.concretes.RealEstateUnitConstructionCost;
+import com.sbm.application.repositories.abstracts.EntityRepository;
+
+@Repository
+public interface RealEstateUnitConstructionCostRepository extends EntityRepository<RealEstateUnitConstructionCost> {
+
+}
---
+++ src/main/java/com/sbm/application/repositories/concretes/RealEstateUsageTypeRepository.java
@@ -0,0 +1,11 @@
+package com.sbm.application.repositories.concretes;
+
+import org.springframework.stereotype.Repository;
+
+import com.sbm.application.entities.concretes.RealEstateUsageType;
+import com.sbm.application.repositories.abstracts.EntityRepository;
+
+@Repository
+public interface RealEstateUsageTypeRepository extends EntityRepository<RealEstateUsageType> {
+
+}
