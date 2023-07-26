35bbb4becae5e4f0a71eb5998376bdd9709575f3
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2023-07-26 09:35:59 +0300

    real estate properties not null validation

--- src/main/java/com/sbm/application/entities/concretes/RealEstate.java
+++ src/main/java/com/sbm/application/entities/concretes/RealEstate.java
@@ -5,6 +5,7 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import com.sbm.application.entities.abstracts.Entity;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,22 +14,31 @@ import lombok.EqualsAndHashCode;
 @Table("RealEstates")
 public class RealEstate extends Entity {
 
+	@NotNull(message = "Boş geçilemez!")
 	@Column("UAVT")
 	private String uavt;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("UnitConstructionCostId")
 	private int unitConstructionCostId;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("CityId")
 	private int cityId;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("CustomerId")
 	private int customerId;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("Value")
 	private double value;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("FloorArea")
 	private int floorArea;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("ConstructionYear")
 	private String constructionYear;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("Address")
 	private String address;
+	@NotNull(message = "Boş geçilemez!")
 	@Column("UrbanLocation")
 	private boolean urbanLocated;
 
